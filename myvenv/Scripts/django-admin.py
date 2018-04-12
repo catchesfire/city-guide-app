d0c7f0adbc5b2d5cace28cf4a85da5dd3c6e7070
Manekin97 <rafikdab@gmail.com>
2018-04-12 13:31:22 +0200

    Adding virtual environment

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\studia\semestr iv\rozwiazania szkieletowe w tworzeniu aplikacji www\pracownia specjalistyczna\projekt\city-guide-app\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
