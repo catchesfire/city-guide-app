6d4aea8e51de862063a45f9a341a43a2b6f8e569
Manekin97 <rafikdab@gmail.com>
2018-04-19 12:32:27 +0200

    Added new models

--- CityGuide/city_guide/models.py
+++ CityGuide/city_guide/models.py
@@ -8,20 +8,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class TicketType(models.Model):
-    type_name = models.CharField(max_length=25)
-    discount = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.type_name + " " + str(self.discount) + "%"    
-
-class Ticket(models.Model):
-    ticket_type = models.ForeignKey(TicketType, on_delete=models.CASCADE)
-    price = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.ticket_type.__str__() + " " + str(self.price) + "zl"
-
 class Attraction(models.Model):
     name = models.CharField(max_length=75)
     location = models.CharField(max_length=1000)
@@ -30,8 +16,50 @@ class Attraction(models.Model):
     age_restriction = models.CharField(max_length=25)
     opening_hours = models.CharField(max_length=100)
 
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+class Attraction_Category(models.Model):
+    id_attracion = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)    
 
+class TicketType(models.Model):
+    type_name = models.CharField(max_length=25)
+    discount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.type_name + " " + str(self.discount) + "%"    
+
+class Ticket(models.Model):
+    price = models.IntegerField(default=0)
+    
+    ticket_type = models.ForeignKey(TicketType, on_delete=models.CASCADE)
+    id_attracion = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ticket_type.__str__() + " " + str(self.price) + "zl"
+
+class Order(models.Model):
+    quantity = models.IntegerField(default=1)
+    date = models.DateField()
+
+    id_cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    id_ticket = models.ForeignKey(Ticket, on_delete=models.SET_NULL)
+
+class Cart(models.Model):
+    id_user = models.ForeignKey(Users, on_delete=models.SET_NULL)
+
+class Tour(models.Model):
+    discount = models.IntegerField(default=0)
+    description = models.CharField(max_length=500)
+    route = models.CharField(max_length=1000)
+    date_from = models.DateField()
+    date_to = models.DateField()
+
+    id_user = models.ForeignKey(Users, on_delete=models.SET_NULL)
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12)
+    mail = models.EmailField()
 
     
