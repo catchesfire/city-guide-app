d78def9283b8779ad0c21f500fcad9ae8636e658
Manekin97 <rafikdab@gmail.com>
2018-04-19 20:38:52 +0200

    Deleted discount field from database

--- CityGuide/city_guide/models.py
+++ CityGuide/city_guide/models.py
@@ -16,10 +16,9 @@ class Category(models.Model):
 
 class TicketType(models.Model):
     type_name = models.CharField(max_length=25)
-    discount = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.type_name + " " + str(self.discount) + "%"
+        return self.type_name
 
 class Attraction(models.Model):
     name = models.CharField(max_length=75)
