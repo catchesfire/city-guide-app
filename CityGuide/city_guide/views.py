2d437d15efccbd7817b1556a57cc0a9011de4092
Manekin97 <rafikdab@gmail.com>
2018-05-10 23:18:47 +0200

    Cleaned up get function

--- CityGuide/city_guide/views.py
+++ CityGuide/city_guide/views.py
@@ -15,17 +15,38 @@ class AttractionsView(generic.ListView):
     form_class = FilterForm
     template_name = 'city_guide/attractions.html'
     context_object_name = 'attractions_obj'
-    form = None
+
+    # @todo
+    # opakować sortowanie w forma
+    # dodać forma z wyszukiwaniem
+    # naprawić widok kiedy jest tylko jedna kategoria
+    # jak get nic nie zwróci, to nie wyświetla się navbar
+    # dobrze by było, gdyby form zapamietywał aktualne filtry
 
     def get(self, request, **kwargs):
         form = self.form_class(request.GET)
-        attractions = None
-        time_min = 0
-        time_max = 1000
-        attr_ids = []
-        attr_list = []
 
-        attr_ids_1 = []
+        def GetAttractionsByCategory(category_ids_list):
+            attr_ids = []
+            attr_list = []
+
+            for cat_id in category_ids_list:
+                for obj in Attraction_Category.objects.filter(id_category=cat_id):
+                    attr_list.append(obj)
+
+                for obj in attr_list:
+                    attr_ids.append(obj.id_attraction.id)
+            
+            return attr_ids
+        
+        def GetAttractionsByPrice(price_min, price_max):
+            attr_ids = []
+            
+            for ticket in Ticket.objects.filter(price__gt=price_min).filter(price__lt=price_max):
+                attr_ids.append(ticket.id_attraction.id)
+            
+            return attr_ids
+
 
         if form.is_valid():
             category_ids = [cat_id for cat_id in request.GET.getlist('categories', Category.objects.all())] 
@@ -34,17 +55,7 @@ class AttractionsView(generic.ListView):
             time_min = request.GET.get('time_min', 0)
             time_max = request.GET.get('time_max', 1000)
 
-            for cat_id in category_ids:
-                for obj in Attraction_Category.objects.filter(id_category=cat_id):
-                    attr_list.append(obj)
-
-                for obj in attr_list:
-                    attr_ids.append(obj.id_attraction.id)
-            
-            for ticket in Ticket.objects.filter(price__gt=price_min).filter(price__lt=price_max):
-                attr_ids_1.append(ticket.id_attraction.id)
-
-            attractions = Attraction.objects.filter(id__in=attr_ids).filter(id__in=attr_ids_1).filter(time_minutes__gt=time_min).filter(time_minutes__lt=time_max).order_by('-name')
+            attractions = Attraction.objects.filter(id__in=GetAttractionsByCategory(category_ids)).filter(id__in=GetAttractionsByPrice(price_min, price_max)).filter(time_minutes__gt=time_min).filter(time_minutes__lt=time_max).order_by('-name')
         else:
             attractions = Attraction.objects.all().order_by('-name')
             
